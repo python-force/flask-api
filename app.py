3e14ad50a8695fecd8160477b34e0008997be976
Radovan <studio@pythonforce.com>
2019-01-22 20:57:29 -0500

    flake rate limiting

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, g, jsonify, render_template
+from flask import Flask, render_template
 
 import config
 import models
 from resources.todos import todos_api
+from flask_limiter import Limiter
+from flask_limiter.util import get_ipaddr
 
 app = Flask(__name__)
 app.secret_key = 'sd7sa76v8*&%7asf7656#dsjksadjwaalcma.caskascjhavs'
 app.register_blueprint(todos_api)
 
+limiter = Limiter(app, global_limits=[config.DEFAULT_RATE], key_func=get_ipaddr)
+limiter.limit("40/day")(todos_api)
+limiter.limit(config.DEFAULT_RATE, per_method=True, methods=['post', 'put', 'delete'])(todos_api)
+
 
 @app.route('/')
 def my_todos():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     models.initialize()
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
+import json
 from peewee import *
 from models import Todo
+from app import app
 
 MODELS = [Todo]
 
@@ -11,18 +13,60 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         # Bind model classes to test db. Since we have a complete list of
         # all models, we do not need to recursively bind dependencies.
+        self.client = app.test_client()
         TEST_DB.bind(MODELS, bind_refs=False, bind_backrefs=False)
         TEST_DB.connect()
         TEST_DB.create_tables([Todo], safe=True)
-
-    def test_todo_creation(self):
-        """Create Todo / 1 Entry"""
         Todo.create(
             name='Launch the Space Rocket',
         )
-        todo = Todo.select().get()
 
+    def test_todo_creation(self):
+        """Create Todo / 1 Entry"""
         self.assertEqual(Todo.select().count(), 1)
+
+    def test_todo_listview_get(self):
+        self.response = self.client.get('http://0.0.0.0:8000/api/v1/todos')
+        decoded = json.loads(self.response.data.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(decoded, [{"id": 1, "name": "Launch the Space Rocket"}])
+
+    def test_todo_listview_post(self):
+        sending_data = {'name': 'Rocket Launched'}
+        self.response = self.client.post('http://0.0.0.0:8000/api/v1/todos', data=sending_data)
+        decoded = json.loads(self.response.data.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 201)
+        self.assertEqual(decoded, {"id": 2, "name": "Rocket Launched"})
+
+
+    def test_todo_detailview_get(self):
+        self.response = self.client.get('http://0.0.0.0:8000/api/v1/todos/1')
+        decoded = json.loads(self.response.data.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(decoded, {"id": 1, "name": "Launch the Space Rocket"})
+
+
+    def test_todo_detailview_post(self):
+        sending_data = {'name': 'Rocket Launched Edited'}
+        self.response = self.client.put('http://0.0.0.0:8000/api/v1/todos/1', data=sending_data)
+        decoded = json.loads(self.response.data.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 201)
+        self.assertEqual(decoded, {"id": 1, "name": "Rocket Launched Edited"})
+
+
+    def test_todo_deleteview(self):
+        self.response = self.client.delete('http://0.0.0.0:8000/api/v1/todos/1')
+        decoded=''
+        if self.response.data:
+            decoded = json.loads(self.response.data.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 204)
+        self.assertEqual(decoded, '')
+
 
     def tearDown(self):
         # Not strictly necessary since SQLite in-memory databases only live
